44d40150ccb07c58935286eaa4f41e72312c69a0
qa_engineer <m.sayfutdinovi72@gmail.com>
2026-02-06 15:16:03 +0700

    Фикс on: pull_request

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -14,7 +14,7 @@ def test_post_v1_account():
     account_api = AccountApi(host='http://185.185.143.231:5051')
     login_api = LoginApi(host='http://185.185.143.231:5051')
     mailhog_api = MailhogApi(host='http://185.185.143.231:5025')
-    login = 'breeze77'
+    login = 'breeze78'
     email = f'{login}@mail.ru'
     password = '12345607030'
     json_data = {
