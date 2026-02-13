7cfd1b43b43d8bff75ccfe0d81b1cf44e552b275
qa_engineer <m.sayfutdinovi72@gmail.com>
2026-02-13 12:06:58 +0700

    Исправление замечаний

--- tests/functional/post_v1_account/test_post_v1_account_login.py
+++ tests/functional/post_v1_account/test_post_v1_account_login.py
@@ -34,7 +34,7 @@ def test_post_v1_account_login():
     mailhog_api = MailhogApi(configuration=mailhog_configuration)
 
 
-    login = 'bask'
+    login = 'jhjklgf'
     email = f'{login}@mail.ru'
     password = '12345607030'
     json_data = {
