9819ed1d7c13e7b00bc852f2051ca78812c0137b
qa_engineer <m.sayfutdinovi72@gmail.com>
2026-02-25 10:12:03 +0700

    Добавил проверки PyHamcrest в post_v1_account

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,12 +1,43 @@
+import datetime
+
+from hamcrest import (
+    has_property,
+    starts_with,
+    assert_that,
+    all_of,
+    instance_of,
+    has_properties,
+    equal_to,
+)
+
 
 def test_post_v1_account(account_helper, prepare_user):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
 
-    response = account_helper.register_new_user(login=login,password=password,email=email)
-    print(response.resource)
-    account_helper.user_login(login=login, password=password)
+    account_helper.register_new_user(login=login,password=password,email=email)
+    response = account_helper.user_login(login=login, password=password)
+    assert_that(
+        response, all_of(
+            has_property('resource', has_property('login', starts_with('user'))),
+            has_property('resource', has_property('registration', instance_of(datetime.datetime))),
+            has_property(
+                'resource', has_properties(
+                    {
+                        "rating": has_properties(
+                            {
+                                "enabled": equal_to(True),
+                                "quality": equal_to(0),
+                                "quantity": equal_to(0)
+                            }
+                        )
+                    }
+                )
+            )
+        )
+    )
+    print(response)
 
 
 
